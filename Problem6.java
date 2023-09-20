44b969d4cab256c23c62c7d90cf4da44608e2fd4
VarunPandya07 <varunpandya14057@gmail.com>
2023-09-20 19:11:41 +0530

    pro6

---
+++ Problem6.java
@@ -0,0 +1,13 @@
+import mathpackge.*;
+/*6.Create a package MathPack having class MathDemo with method add() and sub() to
+find addition and subtraction. Create another program and `import package and
+invoke methods.
+*/
+public class Problem6 {
+    public static void main(String[] args) {
+        A a = new A();
+        a.Add(2,3);
+        a.sub(4,4);
+    }
+
+}
